ab510ab2ae52a81a16ff1d870cf377dac8b25354
lirain <lirain115@163.com>
2019-03-29 17:07:05 +0800

    clean code

--- src/main/java/com/xkool/common/constant/UnitDetailTypeIdConstant.java
+++ src/main/java/com/xkool/common/constant/UnitDetailTypeIdConstant.java
@@ -1,19 +1,19 @@
 package com.xkool.common.constant;
 
 public class UnitDetailTypeIdConstant {
-    public static final int HIGH_RISE_BUILDING_ID = 1;
-    public static final int MID_HIGH_BUILDING_ID = 2;
-    public static final int SUPER_HIGH_BUILDING_ID = 3;
-    public static final int MULTISTAGE_WESTERN_STYLE_HOUSE_ID = 4;
-    public static final int WESTERN_STYLE_HOUSE_ID = 5;
-    public static final int WESTERN_STYLE_BUILDING_ID = 6;
-    public static final int WESTERN_STYLE_VILLA_ID = 7;
-    public static final int INDEPENDENT_VILLA_ID = 8;
-    public static final int LIKE_INDEPENDENT_VILLA_ID = 9;
-    public static final int DOUBLE_VILLA_ID = 10;
-    public static final int STACKED_VILLA_ID = 11;
-    public static final int TOWN_HOUSE_ID = 12;
-    public static final int GROUP_VILLA_ID = 13;
-    public static final int LOFT_ID = 14;
-    public static final int SOHO_ID = 15;
+    public static final int HIGH_RISE_BUILDING_UNIT_DETAIL_TYPE_ID = 1;
+    public static final int MID_HIGH_BUILDING_UNIT_DETAIL_TYPE_ID = 2;
+    public static final int SUPER_HIGH_BUILDING_UNIT_DETAIL_TYPE_ID = 3;
+    public static final int MULTISTAGE_WESTERN_STYLE_HOUSE_UNIT_DETAIL_TYPE_ID = 4;
+    public static final int WESTERN_STYLE_HOUSE_UNIT_DETAIL_TYPE_ID = 5;
+    public static final int WESTERN_STYLE_BUILDING_UNIT_DETAIL_TYPE_ID = 6;
+    public static final int WESTERN_STYLE_VILLA_UNIT_DETAIL_TYPE_ID = 7;
+    public static final int INDEPENDENT_VILLA_UNIT_DETAIL_TYPE_ID = 8;
+    public static final int LIKE_INDEPENDENT_VILLA_UNIT_DETAIL_TYPE_ID = 9;
+    public static final int DOUBLE_VILLA_UNIT_DETAIL_TYPE_ID = 10;
+    public static final int STACKED_VILLA_UNIT_DETAIL_TYPE_ID = 11;
+    public static final int TOWN_HOUSE_UNIT_DETAIL_TYPE_ID = 12;
+    public static final int GROUP_VILLA_UNIT_DETAIL_TYPE_ID = 13;
+    public static final int LOFT_UNIT_DETAIL_TYPE_ID = 14;
+    public static final int SOHO_UNIT_DETAIL_TYPE_ID = 15;
 }
--- src/main/java/com/xkool/common/constant/UnitDetailTypeNameConstant.java
+++ src/main/java/com/xkool/common/constant/UnitDetailTypeNameConstant.java
@@ -1,19 +1,19 @@
 package com.xkool.common.constant;
 
 public class UnitDetailTypeNameConstant {
-    public static final String HIGH_RISE_BUILDING_NAME = "高层";
-    public static final String MID_HIGH_BUILDING_NAME = "小高层";
-    public static final String SUPER_HIGH_BUILDING_NAME = "超高层";
-    public static final String MULTISTAGE_WESTERN_STYLE_HOUSE_NAME = "多层";
-    public static final String WESTERN_STYLE_HOUSE_NAME = "洋房";
-    public static final String WESTERN_STYLE_BUILDING_NAME = "洋楼";
-    public static final String WESTERN_STYLE_VILLA_NAME = "洋墅";
-    public static final String INDEPENDENT_VILLA_NAME = "独立";
-    public static final String LIKE_INDEPENDENT_VILLA_NAME = "类独立";
-    public static final String DOUBLE_VILLA_NAME = "双拼";
-    public static final String STACKED_VILLA_NAME = "叠加";
-    public static final String TOWN_HOUSE_NAME = "联排";
-    public static final String GROUP_VILLA_NAME = "合院";
-    public static final String LOFT_NAME = "LOFT";
-    public static final String SOHO_NAME = "SOHO";
+    public static final String HIGH_RISE_BUILDING_UNIT_DETAIL_TYPE_NAME = "高层";
+    public static final String MID_HIGH_BUILDING_UNIT_DETAIL_TYPE_NAME = "小高层";
+    public static final String SUPER_HIGH_BUILDING_UNIT_DETAIL_TYPE_NAME = "超高层";
+    public static final String MULTISTAGE_WESTERN_STYLE_HOUSE_UNIT_DETAIL_TYPE_NAME = "多层";
+    public static final String WESTERN_STYLE_HOUSE_UNIT_DETAIL_TYPE_NAME = "洋房";
+    public static final String WESTERN_STYLE_BUILDING_UNIT_DETAIL_TYPE_NAME = "洋楼";
+    public static final String WESTERN_STYLE_VILLA_UNIT_DETAIL_TYPE_NAME = "洋墅";
+    public static final String INDEPENDENT_VILLA_UNIT_DETAIL_TYPE_NAME = "独立";
+    public static final String LIKE_INDEPENDENT_VILLA_UNIT_DETAIL_TYPE_NAME = "类独立";
+    public static final String DOUBLE_VILLA_UNIT_DETAIL_TYPE_NAME = "双拼";
+    public static final String STACKED_VILLA_UNIT_DETAIL_TYPE_NAME = "叠加";
+    public static final String TOWN_HOUSE_UNIT_DETAIL_TYPE_NAME = "联排";
+    public static final String GROUP_VILLA_UNIT_DETAIL_TYPE_NAME = "合院";
+    public static final String LOFT_UNIT_DETAIL_TYPE_NAME = "LOFT";
+    public static final String SOHO_UNIT_DETAIL_TYPE_NAME = "SOHO";
 }
