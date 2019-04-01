29d4d069079d5f7e26c78676d3154a3ba4864d62
lirain <lirain115@163.com>
2019-04-01 10:24:48 +0800

    half code

---
+++ src/main/java/com/xkool/common/constant/PlanningLandUsageNameConstant.java
@@ -0,0 +1,17 @@
+package com.xkool.common.constant;
+
+public class PlanningLandUsageNameConstant {
+    public static final String RESIDENTIAL_PLANNING_LAND_USAGE_NAME = "居住用地";
+    public static final String TYPE_ONE_RESIDENTIAL_PLANNING_LAND_USAGE_NAME = "一类居住用地";
+    public static final String TYPE_ONE_RESIDENTIAL_AND_FACILITY_PLANNING_LAND_USAGE_NAME = "一类住宅用地";
+    public static final String TYPE_ONE_SERVICE_FACILITY_PLANNING_LAND_USAGE_NAME = "一类服务设施用地";
+    public static final String TYPE_TWO_RESIDENTIAL_PLANNING_LAND_USAGE_NAME = "二类居住用地";
+    public static final String TYPE_TWO_RESIDENTIAL_AND_FACILITY_PLANNING_LAND_USAGE_NAME = "二类住宅用地";
+    public static final String TYPE_TWO_SERVICE_FACILITY_PLANNING_LAND_USAGE_NAME = "二类服务设施用地";
+    public static final String TYPE_THREE_RESIDENTIAL_PLANNING_LAND_USAGE_NAME = "三类居住用地";
+    public static final String TYPE_THREE_RESIDENTIAL_AND_FACILITY_PLANNING_LAND_USAGE_NAME = "三类住宅用地";
+    public static final String TYPE_THREE_SERVICE_FACILITY_PLANNING_LAND_USAGE_NAME = "三类服务设施用地";
+    public static final String BUSINESS_AND_FACILITY_PLANNING_LAND_USAGE_NAME = "商业服务业设施用地";
+    public static final String BUSINESS_PLANNING_LAND_USAGE_NAME = "商业用地";
+    public static final String RETAIL_BUSINESS_PLANNING_LAND_USAGE_NAME = "零售商业用地";
+}
