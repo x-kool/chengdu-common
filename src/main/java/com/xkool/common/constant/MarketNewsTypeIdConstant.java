a03d05e6bd09d97e97b9c98b3fdcea6fca910c9c
lirain <lirain115@163.com>
2019-04-08 10:46:13 +0800

    add market news type

---
+++ src/main/java/com/xkool/common/constant/MarketNewsTypeIdConstant.java
@@ -0,0 +1,7 @@
+package com.xkool.common.constant;
+
+public class MarketNewsTypeIdConstant {
+    public static final int ESTATE_MARKET_POLICY_MARKET_NEWS_TYPE_ID = 1;
+    public static final int MONTHLY_PAYMENTS_SITUATION_MARKET_NEWS_TYPE_ID = 2;
+    public static final int DEAL_SITUATION_MARKET_NEWS_TYPE_ID = 3;
+}
